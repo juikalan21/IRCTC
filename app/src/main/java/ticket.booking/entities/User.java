b35b1b923062ca935ed59b8695ea47ad3c196ebc
Jui Kalan <juikalan21@gmail.com>
2025-02-18 13:24:04 +0530

    first commit

---
+++ app/src/main/java/ticket.booking/entities/User.java
@@ -0,0 +1,17 @@
+package ticket.booking.entities;
+
+import java.util.List;
+
+import com.google.common.base.Ticker;
+
+public class User 
+{
+    private String name;
+    private String password;
+    private String hashPassword;
+    private List<Ticket> ticketsBooked;
+    private String userId;
+    
+
+
+}
